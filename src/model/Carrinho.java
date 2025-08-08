032425dbcb2d9985221165e828cecb69105622cb
Matheus Gomes de Moura <Matheus.gomesdemouraa@gmail.com>
2025-08-08 14:36:50 -0300

    add: ItemCarrinho

--- src/model/Carrinho.java
+++ src/model/Carrinho.java
@@ -2,19 +2,37 @@ package model.enums;
 
 class Carrinho {
     private Cliente cliente;
-    private List<Produto> produtos;
+    private List<ItemCarrinho> itens;
 
-    public Carrinho(Cliente cliente) {
-        this.cliente = cliente;
-        this.produtos = new ArrayList<>();
+    public CarrinhoDeCompras() {
+        this.itens = new ArrayList<>();
     }
 
-    public void adicionarProduto(Produto p) { produtos.add(p); }
+    public void adicionarItem(Produto produto, int quantidade) {
+        // Se o produto já existir no carrinho, soma quantidade
+        for (ItemCarrinho item : itens) {
+            if (item.getProduto().equals(produto)) {
+                item.atualizarQuantidade(item.getQuantidade() + quantidade);
+                return;
+            }
+        }
+        itens.add(new ItemCarrinho(produto, quantidade));
+    }
+
+    public void removerItem(Produto produto) {
+        itens.removeIf(item -> item.getProduto().equals(produto));
+    }
+
     public double calcularTotal() {
-        return produtos.stream().mapToDouble(Produto::getValor).sum();
+        return itens.stream().mapToDouble(ItemCarrinho::subTotal).sum();
     }
 
-    public void listarProdutos() {
-        produtos.forEach(System.out::println);
+    public void listarItens() {
+        if (itens.isEmpty()) {
+            System.out.println("Carrinho vazio.");
+        } else {
+            itens.forEach(System.out::println);
+            System.out.println("TOTAL: R$ " + calcularTotal());
+        }
     }
 }
